6a565bc5dd76a7daad59558b65683ea8fecb6284
RidhomTeto <ridhomteto@gmail.com>
2024-03-16 18:09:25 +0200

    16.03.2024
    Day3

---
+++ C#/ExerciseDay2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+
+
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hellow world");
+        string studentName = "Fatimah";
+        Console.WriteLine(studentName);
+        int studentAge;
+        Console.Write("Input student age :");
+        studentAge = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Student age is " + studentAge);
+
+        Console.Write("Is there any student : ");
+        bool studentExists = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("It is " + studentExists);
+
+        int math, drawing, finnish, bengali;
+        Console.Write("Marks in math : ");
+        math = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("Marks in drawing : ");
+        drawing = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Marks in finnish : ");
+        finnish = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Marks in bengali : ");
+        bengali = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(math + drawing + finnish + bengali);
+
+
+
+    }
+}
---
+++ C#/variableExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿class Variable
+{
+    public static void Main(String[] args)
+    {
+        string name = "Apple Iphone 11 pro";
+        double price = 999.99;
+        string catagory = "IOS";
+        bool available = true;
+        int sold = 5;
+        Console.WriteLine(name);
+        Console.WriteLine(price);
+        Console.WriteLine(catagory);
+        Console.WriteLine(available);
+        Console.WriteLine(sold);
+    }
+}
